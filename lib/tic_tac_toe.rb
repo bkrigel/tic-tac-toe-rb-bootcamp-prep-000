1b720cce779f7fdecf2669221d3e6f943336e63f
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-03 00:25:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,13 +8,32 @@ def display_board(board)
   puts  " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(input)
  index = input.to_i - 1
 end
 
+
 def move(board, index, character="X")
   board[index] = character
 end
+
+
+def valid_move?(board, index)
+    if index.between?(0,8) && !position_taken?(board,index)
+      true
+    end
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    return false
+  else
+    return true
+  end
+end
+
 
 
 
