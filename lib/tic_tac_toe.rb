75b275bcd1820408ca5a8ed9013f32ba92e6c64a
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-03 00:24:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,6 +8,16 @@ def display_board(board)
   puts  " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+ index = input.to_i - 1
+end
+
+def move(board, index, character="X")
+  board[index] = character
+end
+
+
+
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
